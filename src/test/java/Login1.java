3388e63040337534fb8a5a2ecb8ec985a613e2b8
Ashutosh Ranjan <ashutosh16122023@gmail.com>
2026-05-28 07:29:25 +0530

    second commit

--- src/test/java/Login1.java
+++ src/test/java/Login1.java
@@ -1,5 +1,6 @@
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -15,6 +16,11 @@ public class Login1 {
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 			driver.get("https://tutorialsninja.com/demo/");
+			driver.findElement(By.xpath("//span[text()='My Account']")).click();
+			driver.findElement(By.linkText("Login")).click();
+			driver.findElement(By.id("input-email")).sendKeys("ashutosh16122023@gmail.com");
+			driver.findElement(By.id("input-password")).sendKeys("7798652341");
+			driver.findElement(By.xpath("//input[@value='Login']")).click();)
 			
 		}
 
---
+++ src/test/java/Login2.java
@@ -0,0 +1,39 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Login2 {
+	@Test
+	public void verifyLoginWithValidCredentials() {
+
+		WebDriver driver = new ChromeDriver();
+
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+
+	    driver.get("https://tutorialsninja.com/demo/");
+
+	    driver.findElement(By.xpath("//span[text()='My Account']")).click();
+
+	    driver.findElement(By.linkText("Login")).click();
+
+	    driver.findElement(By.id("input-email"))
+	          .sendKeys("ashutosh16122023@gmail.com");
+
+	    driver.findElement(By.id("input-password"))
+	          .sendKeys("7798652341");
+
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	    wait.until(ExpectedConditions.elementToBeClickable(
+	            By.xpath("//input[@value='Login']")))
+	        .click();
+	}
+
+}
